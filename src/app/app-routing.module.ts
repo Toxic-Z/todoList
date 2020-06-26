d30e38adf4219b566e3eb159a865539c626a6a77
Roman <romakingforever@gmail.com>
2020-06-26 12:08:03 +0300

    skeleton done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { Page404Component } from './components/page404/page404.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, Page404Component],
+  exports: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
